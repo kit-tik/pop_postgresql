5d9a3e693c362a7b1eef53d80ef22fc7e9c9442b
kit-tik <141170868+kit-tik@users.noreply.github.com>
2023-08-21 16:15:57 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 import Slider from '@/components/Slider'
 import Image from 'next/image'
 
-export default function Home({
-  params,
-  searchParams,
-}: {
-  params: { slug: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
+export default function Home() {
   return (
     <main>
       <Slider/>
